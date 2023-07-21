fdb452541d6d02b9a61223f958192a87a8a6675a
DoTanDuong <dotanduong17@gmail.com>
2023-07-21 07:41:58 +0700

    Summary: add factor blade

---
+++ resources/views/factor.blade.php
@@ -0,0 +1,11 @@
+@extends('mails.layout')
+@section('content')
+    Xin chào {{ $name }}!
+    <span class="block mb-15">Mã xác thực của bạn là:</span>
+    <p class="backup-code-block">
+        Mã xác thực: <span class="backup-code"> {{ $code }} </span>
+    </p>
+    <br/>
+    <span class="block">Regards,</span>
+    Factor
+@endsection

97ae2439d65f60e7593f4bfef385a42eaeac4282
DoTanDuong <dotanduong17@gmail.com>
2023-07-22 00:55:42 +0700

    add verify email blade

---
+++ resources/views/verify.email.blade.php
@@ -0,0 +1,9 @@
+@extends('mails.layout')
+@section('content')
+    Xin chào {{ $name }}!
+    <span class="block mb-15">Vui lòng nhấp vào nút bên dưới để xác minh địa chỉ email của bạn.</span>
+    <a href="{{ $url }}" class="btn--primary">Xác nhận địa chỉ email</a>
+    <br/>
+    <span class="block">Regards,</span>
+    MaxCloud
+@endsection

573c49f22ba2069544f6457fc38e047115ca3f71
DoTanDuong <dotanduong17@gmail.com>
2023-08-03 07:34:11 +0700

    add svg blade

---
+++ resources/views/svgStatistics.blade.php
@@ -0,0 +1,23 @@
+<svg viewBox="0 0 36 36" class="circular-chart">
+    <path class="circle-off"
+          stroke="#f1faff"
+          stroke-dasharray="100, 100"
+          d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
+    />
+    <path class="circle"
+          stroke="{{ $color }}"
+          stroke-dasharray="100, 100"
+          d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
+    />
+    <path class="circle-off"
+          stroke="#f1faff"
+          stroke-dasharray="100, 100"
+          d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
+    />
+    <path class="circle"
+          stroke="{{ $color }}"
+          stroke-dasharray="100, 100"
+          d="M18 2.254 a 15.8799 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
+    />
+    <text class="small" x="16" y="20">{{ $number }}</text>
+</svg>

210dd90e9a22ab3daae3b72bda3752b18debdbbf
DoTanDuong <dotanduong17@gmail.com>
2023-08-18 00:31:50 +0700

    add more icon sort

--- resources/views/sort_header_table.blade.php
+++ resources/views/sort_header_table.blade.php
@@ -15,6 +15,18 @@
                 @endif
             </span>
         @endif
+
+        @if($isSort)
+            <span class="table__sort">
+                @if(request()->input('sort_by') == $col)
+                    @if(request()->input('sort_type', 'asc') == 'asc')
+                        <i class="fas fa-arrow-down"></i>
+                    @else
+                        <i class="fas fa-arrow-up"></i>
+                    @endif
+                @endif
+            </span>
+        @endif
     </th>
 @endforeach
 

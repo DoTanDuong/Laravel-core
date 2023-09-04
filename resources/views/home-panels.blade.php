17ecb6310ba5584e7075152ab7ffc4294ba8ed5b
DoTanDuong <dotanduong17@gmail.com>
2023-09-04 07:49:27 +0700

    add more announcements

--- resources/views/home-panels.blade.php
+++ resources/views/home-panels.blade.php
@@ -57,6 +57,33 @@
                     @endforeach
                 </div>
             </div>
+
+            <div class="box rounded-0 box--success mb-4">
+                <div class="box__header text--gray d-flex align-items-center justify-content-between">
+                    <h5 class="mb-0 flex-70">
+                        <i class="far fa-newspaper"></i>
+                        {{__('Tin Tức')}}
+                    </h5>
+                    <a href="{{ route('account.news') }}" class="btn button button--success flex-30">
+                        <i class="fas fa-plus"></i>
+                        {{ __('Xem tất cả') }}
+                    </a>
+                </div>
+                <div>
+                    @foreach($announcements as $announcement)
+                        <div class="home-panels__item">
+                            <a href="{{ route('account.news.detail', ['id' => $announcement->id]) }}" class="home-panels__link">
+                                <div class="home-panels__title">
+                                    {{ $announcement->title }}.
+                                    <div class="home-panels__time">
+                                        {{ date('d-m-Y (H:s)', strtotime($announcement->date)) }}.
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </section>

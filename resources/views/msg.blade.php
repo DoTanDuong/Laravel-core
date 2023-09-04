961a24f84859fe60d27d0bc270f6aad29e7b3df5
DoTanDuong <dotanduong17@gmail.com>
2023-09-05 00:22:55 +0700

    remove session msg

--- resources/views/msg.blade.php
+++ resources/views/msg.blade.php
@@ -18,15 +18,6 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             </div>
-
-            <div class="col-12">
-                <div class="alert alert-{{ $msg }} alert-dismissible fade show" role="alert">
-                    <div class="alert-message">
-                        {!! Session::get($msg) !!}
-                    </div>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            </div>
         </div>
     @endif
 @endforeach
